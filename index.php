f08810b8ab795269554cb9a99d229b2f2fea686b
FelixvL <digitaleservice@gmail.com>
2017-10-31 14:25:21 +0100

    layout made

--- index.php
+++ index.php
@@ -7,5 +7,11 @@
         <script src="oefening1.js"></script>
     </head>
     <body>
+        <table border=1>
+            <tr><td><div>X</div></td><td><div>X</div></td><td><div>X</div></td></tr>
+            <tr><td><div>X</div></td><td><div>X</div></td><td><div>X</div></td></tr>
+            <tr><td><div>X</div></td><td><div>X</div></td><td><div>X</div></td></tr>
+        </table>
+        
     </body>
 </html>
