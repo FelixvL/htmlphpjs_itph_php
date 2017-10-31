d1965769a7e354fbcc01d8f78ee028073abeccd5
FelixvL <digitaleservice@gmail.com>
2017-10-31 14:46:10 +0100

    starting with empty fields

--- index.php
+++ index.php
@@ -9,19 +9,19 @@
     <body>
         <table border=1>
             <tr>
-                <td onclick="inputPlayer(1)"><div id="vakje1">X</div></td>
-                <td onclick="inputPlayer(2)"><div id="vakje2">X</div></td>
-                <td onclick="inputPlayer(3)"><div id="vakje3">X</div></td>
+                <td onclick="inputPlayer(1)"><div id="vakje1"></div></td>
+                <td onclick="inputPlayer(2)"><div id="vakje2"></div></td>
+                <td onclick="inputPlayer(3)"><div id="vakje3"></div></td>
             </tr>
             <tr>
-                <td onclick="inputPlayer(4)"><div id="vakje4">X</div></td>
-                <td onclick="inputPlayer(5)"><div id="vakje5">X</div></td>
-                <td onclick="inputPlayer(6)"><div id="vakje6">X</div></td>
+                <td onclick="inputPlayer(4)"><div id="vakje4"></div></td>
+                <td onclick="inputPlayer(5)"><div id="vakje5"></div></td>
+                <td onclick="inputPlayer(6)"><div id="vakje6"></div></td>
             </tr>
             <tr>
-                <td onclick="inputPlayer(7)"><div id="vakje7">X</div></td>
-                <td onclick="inputPlayer(8)"><div id="vakje8">X</div></td>
-                <td onclick="inputPlayer(9)"><div id="vakje9">X</div></td>
+                <td onclick="inputPlayer(7)"><div id="vakje7"></div></td>
+                <td onclick="inputPlayer(8)"><div id="vakje8"></div></td>
+                <td onclick="inputPlayer(9)"><div id="vakje9"></div></td>
             </tr>
         </table>
         
