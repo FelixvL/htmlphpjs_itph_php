dba69fd958de712a42e01f30d7c49cf771850130
FelixvL <digitaleservice@gmail.com>
2017-10-31 14:07:10 +0100

    eerste start

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $nameGuest = "Jopie";
+?>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="oefening1.css">
+        <script src="oefening1.js"></script>
+    </head>
+    <body>
+        <input type="button" onclick="test(1)" value="1"/>
+        <input type="button" onclick="test(2)" value="2"/>
+
+
+        <p id="example">
+            This is my tekst in example p
+        </p>
+        <input type="text" id="result">
+
+    </body>
+</html>
---
+++ oefening1.js
@@ -0,0 +1,19 @@
+function test(variable){
+    alert(variable);
+    document.getElementById('result').value = variable;
+/*
+    var getal = 6;
+    
+    console.log('My debug tekst');
+    console.log(getal);
+    
+    
+    var someElement = document.getElementById('example');
+    alert(someElement);
+    console.log(someElement);
+    someElement.innerHTML = 'new Tekst';
+    
+    var myBody = document.getElementsByTagName('html');
+    console.log(myBody[0]);
+*/
+}
