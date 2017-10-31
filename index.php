a171d20bd95e2ab6e7ebbd50a34e0b2531386f87
FelixvL <digitaleservice@gmail.com>
2017-10-31 14:18:20 +0100

    alles leeg

--- index.php
+++ index.php
@@ -7,14 +7,5 @@
         <script src="oefening1.js"></script>
     </head>
     <body>
-        <input type="button" onclick="test(1)" value="1"/>
-        <input type="button" onclick="test(2)" value="2"/>
-
-
-        <p id="example">
-            This is my tekst in example p
-        </p>
-        <input type="text" id="result">
-
     </body>
 </html>
